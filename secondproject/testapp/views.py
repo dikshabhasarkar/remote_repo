2fffe6b1afd7ba324170f1229041b72d2652fc1a
dikpython <dikshabhasarkar@gmail>
2021-10-17 08:27:26 +0530

    first commit

---
+++ secondproject/testapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+# Create your views here.
+ 
+def timeinfo(request):
+    date=datetime.datetime.now()
+    msg='<h1>Hello Good Morning<h1>'
+    msg=msg+'<h1>Now server time is:-'+ str(date)+'<h1>'
+    return HttpResponse(msg)
+
+# def hydjob
